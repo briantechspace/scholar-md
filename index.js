5725742bb148042d8e0ed4a9ba44922d34426ad3
brayo-tech <oumabriansoja@gmail.com>
2025-12-22 12:48:05 -0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import "./server.js";
-import { startBot } from "./bot.js";
+const app = require("./server");
+const { startBot } = require("./bot");
 
+const PORT = process.env.PORT || 3000;
+
+// Start HTTP server first
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
+// Start bot in idle mode (no pairing yet)
 startBot();
