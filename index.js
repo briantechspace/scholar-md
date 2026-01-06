9a432c02761ec8ad5718b1036aa76ae07f6fe5a6
brayo-tech <oumabriansoja@gmail.com>
2026-01-06 03:11:34 -0900

    Remove bot startup logic from index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
 require("./server");
-
-// Only start bot if file exists
-try {
-  const { startBot } = require("./bot");
-  if (typeof startBot === "function") {
-    startBot();
-  }
-} catch (e) {
-  console.log("Bot not started (bot.js missing or disabled)");
-}
